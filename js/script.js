d5b3247c0cfb74833a2c7752f907a7f2d2f87d4a
alecvd <14879968+AlecVD@users.noreply.github.com>
2022-11-09 15:00:16 -0800

    add computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("Rock Paper Scissors Game")
+
+const choices = ["Rock","Paper","Scissors"];
+
+function getComputerChoice(){
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+console.log(getComputerChoice());
