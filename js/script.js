cb73866991eddd110c9bc2cb3728fb42ad85551a
alecvd <14879968+AlecVD@users.noreply.github.com>
2022-12-06 21:35:07 -0800

    fix onclick functions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 console.log("Rock Paper Scissors Game")
-alert("Rock Paper Scissors")
 
 const choices = ["Rock","Paper","Scissors"]
+var computerWins = 0
+var playerWins = 0
 
 function getComputerChoice(){
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-document.getElementById("rock").addEventListener("click",function(){playRound("Rock",getComputerChoice())})
-document.getElementById("rock").addEventListener("click",function(){playRound("Rock",getComputerChoice())})
-document.getElementById("rock").addEventListener("click",function(){playRound("Rock",getComputerChoice())})
+document.getElementById("rock").onclick = function(){playerChoice("Rock")}
+document.getElementById("paper").onclick = function(){playerChoice("Paper")}
+document.getElementById("scissors").onclick = function(){playerChoice("Scissors")}
 
 function playRound(playerChoice,computerChoice){
     switch(true){
@@ -19,11 +20,13 @@ function playRound(playerChoice,computerChoice){
         case playerChoice === "Rock" && computerChoice === "Scissors":
         case playerChoice === "Paper" && computerChoice === "Rock":
         case playerChoice === "Scissors" && computerChoice === "Paper":
+            playerWins += 1
             return "Player Wins"
             break;
         case playerChoice === "Rock" && computerChoice === "Paper":
         case playerChoice === "Paper" && computerChoice === "Scissors":
         case playerChoice === "Scissors" && computerChoice === "Rock":
+            computerWins += 1
             return "Computer Wins"
             break;
         default:
@@ -31,4 +34,12 @@ function playRound(playerChoice,computerChoice){
     }
 }
 
+function playerChoice(choice){
+    var computer = getComputerChoice()
+    var result = playRound(choice,computer)
+    document.getElementById("result").innerHTML = "Result: " + result;
+    document.getElementById("player").innerHTML = "Player Choice: " + choice;
+    document.getElementById("computer").innerHTML = "Computer Choice: " + computer;
+    document.getElementById("score").innerHTML = "Player: " + playerWins + " Computer: " + computerWins
+}
 //console.log(playRound(getPlayerChoice(),getComputerChoice()))
