2aec1f0ca936431641d464d2844fcbdaaa35524d
alecvd <14879968+AlecVD@users.noreply.github.com>
2022-12-06 17:31:17 -0800

    add playRound function

--- js/script.js
+++ js/script.js
@@ -9,5 +9,24 @@ function getComputerChoice(){
 console.log(getComputerChoice());
 
 function playRound(playerChoice,computerChoice){
+    switch(true){
+        case playerChoice === computerChoice:
+            return "Tie";
+            break;
+        case playerChoice === "Rock" && computerChoice === "Scissors":
+        case playerChoice === "Paper" && computerChoice === "Rock":
+        case playerChoice === "Scissors" && computerChoice === "Paper":
+            return "Player Wins"
+            break;
+        case playerChoice === "Rock" && computerChoice === "Paper":
+        case playerChoice === "Paper" && computerChoice === "Scissors":
+        case playerChoice === "Scissors" && computerChoice === "Rock":
+            return "Computer Wins"
+            break;
+        default:
+            console.log("?")
+    }
 
 }
+
+console.log(playRound(prompt("Player Input"),getComputerChoice()))
